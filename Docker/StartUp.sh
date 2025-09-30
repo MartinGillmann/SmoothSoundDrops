d57e47afffcd426b707c20c29af64dafbc1aa861
Martin Gillmann <gillmann_martin@yahoo.com>
2025-09-30 12:31:54 +0200

    Pre Backend-Use

--- Docker/StartUp.sh
+++ Docker/StartUp.sh
@@ -1,33 +1,34 @@
-echo "In StartUp.sh ..." >> /var/startup.log
+echo "$(date) *** In StartUp.sh ..." >> /var/startup.log
 
-echo "Pwd is $(pwd)" >> /var/startup.log
+echo "$(date) *** Pwd is $(pwd)" >> /var/startup.log
 
 chmod +x ReplacePlaceholders.sh
 dos2unix ReplacePlaceholders.sh
-echo "LS is $(ls -als)" >> /var/startup.log
+echo "$(date) *** LS is $(ls -als)" >> /var/startup.log
 
 ./ReplacePlaceholders.sh
-echo "ReplacePlaceholders done" >> /var/startup.log
-echo "LS is $(ls -als)" >> /var/startup.log
+echo "$(date) *** ReplacePlaceholders done" >> /var/startup.log
+echo "$(date) *** LS is $(ls -als)" >> /var/startup.log
 
-echo "Json is $(cat ../smoothsounddrops/package.json)" >> /var/startup.log
+echo "$(date) *** Json is $(cat ../smoothsounddrops/package.json)" >> /var/startup.log
 
-echo "'npm run build' ..." >> /var/startup.log
+echo "$(date) *** 'npm run build' ..." >> /var/startup.log
 cd ../smoothsounddrops
 npm run build
-echo "'npm run build' done" >> /var/startup.log
+echo "$(date) *** 'npm run build' done" >> /var/startup.log
 cd ..
 
-echo "Pwd is $(pwd)" >> /var/startup.log
-echo "nginxConfig.txt ..." >> /var/startup.log
+echo "$(date) *** Pwd is $(pwd)" >> /var/startup.log
+echo "$(date) *** nginxConfig.txt ..." >> /var/startup.log
 dos2unix Docker/nginxConfig.txt
 cp Docker/nginxConfig.txt /etc/nginx/conf.d/default.conf
 
-echo "NGINX: $(nginx -T)" >> /var/startup.log
+echo "$(date) *** NGINX: $(nginx -T)" >> /var/startup.log
 
 
-echo "Starting nginx ..." >> /var/startup.log
+echo "$(date) *** Starting nginx ..." >> /var/startup.log
+echo "$(date) *** Now you are ready to cUrl into it :-)" >> /var/startup.log
 nginx -g "daemon off;"
 
-echo "StartUp.sh done" >> /var/startup.log
+echo "$(date) *** StartUp.sh done" >> /var/startup.log
 
--- README.md
+++ README.md
@@ -6,11 +6,21 @@ It displays music notes so that I can play it on the piano.
 
 ## Deployment
 Follow the instructions in the Docker file.
-Then open in with http://<ip>:8089/smoothsounddrops/
+Then open in with http://__YOUR_IP__:8089/smoothsounddrops/
 
 
 ## Still to come:
 - Remove hardcoded notes and geather them from the server
 - Make the Docker deployment to host it on my Raspberry Pi
 - Make it look nice by adding css
+
+## Folgende Sub-Ordner sind wichtig
+- docker
+- public
+- src
+
+## Folgende Sub-Ordner können gelöscht werden
+- node_modules (wird durch 'npm install' erzeugt)
+- build        (wird durch 'npm run build' erzeugt)
+
 
