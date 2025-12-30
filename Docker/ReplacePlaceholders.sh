e51060eb950040235acc621f6c2386e750115ea7
MartinGillmann <gillmann_martin@yahoo.com>
2025-12-30 11:36:44 +0100

    Refactor placeholder replacement in ReplacePlaceholders.sh

--- Docker/ReplacePlaceholders.sh
+++ Docker/ReplacePlaceholders.sh
@@ -1,32 +1,11 @@
 
-echo "sed -i 's_\"homepage\": \"localhost\"_    \"homepage\":\"pl-protocol;://pl-domain;/pl-path;\"_' ../smoothsounddrops/package.json" > _sed.sh
+echo " cd ../smoothsounddrops \n npm pkg set 'homepage'='/$PLACEHOLDER_PATH' \n cat package.json \n cd ../Docker" | sed 's/"//g' > _sed.sh
 chmod +x _sed.sh
 echo "_sed(1): $(cat _sed.sh)" >> /var/startup.log
 ./_sed.sh
-
-echo "sed -i 's*pl-protocol;*$PLACEHOLDER_PROTOCOL*' ../smoothsounddrops/package.json" > _sed.sh
-chmod +x _sed.sh
-echo "_sed(2): $(cat _sed.sh)" >> /var/startup.log
-./_sed.sh
-
-echo "sed -i 's*pl-domain;*$PLACEHOLDER_DOMAIN*' ../smoothsounddrops/package.json" > _sed.sh
-chmod +x _sed.sh
-echo "_sed(3): $(cat _sed.sh)" >> /var/startup.log
-./_sed.sh
-
-echo "sed -i 's*pl-path;*$PLACEHOLDER_PATH*' ../smoothsounddrops/package.json" > _sed.sh
-chmod +x _sed.sh
-echo "_sed(4): $(cat _sed.sh)" >> /var/startup.log
-./_sed.sh
-
-
-echo "sed -i 's*ph-server-name*$PLACEHOLDER_DOMAIN*' ../Docker/nginxConfig.txt" > _sed.sh
-chmod +x _sed.sh
-echo "_sed(5): $(cat _sed.sh)" >> /var/startup.log
-./_sed.sh
-
 
 echo "sed -i 's*ph-smooth-path*$PLACEHOLDER_PATH*' ../Docker/nginxConfig.txt" > _sed.sh
 chmod +x _sed.sh
 echo "_sed(6): $(cat _sed.sh)" >> /var/startup.log
 ./_sed.sh
+
